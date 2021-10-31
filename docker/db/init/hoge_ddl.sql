43c2697d7328471e9961c3a19873eb54ea48f870
Yuki Watanabe <>
2021-11-01 00:54:27 +0900

    fix: docker-compose.yml, Dockerfileの修正

--- docker/db/init/hoge_ddl.sql
+++ docker/db/init/hoge_ddl.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS hoge (
+create TABLE IF NOT EXISTS go_database.hoge (
 `hoge_id` int(11) NOT NULL,
-`hogehoge` varchar NOT NULL,
+`hogehoge` text NOT NULL,
 `created_at` DATETIME,
 `update_at` DATETIME
 );
