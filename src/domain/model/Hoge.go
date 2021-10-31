ccabf9cd58aea5a134bb29da14a6ceb4eb8d6dfe
Yuki Watanabe <>
2021-11-01 01:28:06 +0900

    fix: structのField名の修正

--- src/domain/model/Hoge.go
+++ src/domain/model/Hoge.go
@@ -4,23 +4,15 @@ import "time"
 
 type Hoge struct {
 	hogeId    int        `json:"hoge_id"gorm:"primary_key"`
-	hogehoge  string     `json:"hogehoge"`
-	createdAt time.Time  `json:"created_at"`
-	updatedAt time.Time  `json:"update_at"`
-	deletedAt *time.Time `sql:"index"json:"-"`
+	Hogehoge  string     `json:"hogehoge"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"update_at"`
+	DeletedAt *time.Time `sql:"index"json:"-"`
 }
 
 func NewHoge(hogehoge string) *Hoge {
 	hoge := new(Hoge)
-	hoge.hogehoge = hogehoge
+	hoge.Hogehoge = hogehoge
 
 	return hoge
-}
-
-func (hoge Hoge) getHogehoge() string {
-	return hoge.hogehoge
-}
-
-func (hoge *Hoge) setHogehoge(hogehoge string) {
-	hoge.hogehoge = hogehoge
 }
